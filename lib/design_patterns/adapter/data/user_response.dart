91a6d4efc847478839ba0001ac64da55d17c792e
Alvaro Armijos <alvaro.armijos@zemoga.com>
2022-12-31 17:12:51 -0500

    Create UserResponse on Adapter.

---
+++ lib/design_patterns/adapter/data/user_response.dart
@@ -0,0 +1,19 @@
+class UserResponse {
+  final String id;
+  final String userName;
+  final String address;
+  final String career;
+  final String token;
+  final String userPhotoUrl;
+  final String tokenTransfer;
+
+  UserResponse(
+    this.id,
+    this.userName,
+    this.address,
+    this.career,
+    this.token,
+    this.userPhotoUrl,
+    this.tokenTransfer,
+  );
+}
