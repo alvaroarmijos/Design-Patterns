d29d8f7cea9369e50aabcf851ed9bb5f1d24842a
Alvaro Armijos <alvaro.armijos@zemoga.com>
2022-12-31 17:13:10 -0500

    Create UserViewModel on Adapter.

---
+++ lib/design_patterns/adapter/view/user_view_model.dart
@@ -0,0 +1,13 @@
+class UserViewModel {
+  final String userName;
+  final String userPhotoUrl;
+  final String address;
+  final String career;
+
+  UserViewModel(
+    this.userName,
+    this.userPhotoUrl,
+    this.address,
+    this.career,
+  );
+}
